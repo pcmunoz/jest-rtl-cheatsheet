a2bc4fe509e1f31813c981e5ff1d256aa04672ef
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2022-03-11 14:55:42 +0800

    Add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import './index.css';
 
 const container = document.getElementById('root');
 
+if (!container) throw new Error('Failed to find the root element.')
+
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
 
